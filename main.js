3c96c7b9f60a5cfb7a620901a95d11c652162dbb
ianpenalver <jabezpenalver2k20@gmail.com>
2023-05-03 00:20:34 +0800

    Added About Window

--- main.js
+++ main.js
@@ -1,14 +1,89 @@
 // Imported Modules
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
 const axios = require('axios');
 const dotenv = require('dotenv').config();
 
-// Main Window
+// Global Variables
 const isDev = true;
+const isMac = process.platform === 'darwin';
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'About',
+        click: aboutWindow
+      },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'minimize' },
+      { type: 'separator' },
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  }
+];
 
+// Main Window
 const createWindow = () => {
-  const win = new BrowserWindow({
+  const main = new BrowserWindow({
     width: isDev ? 1200 : 600,
     height: 600,
     webPreferences: {
@@ -18,19 +93,36 @@ const createWindow = () => {
     },
   });
 
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   if (isDev) {
-    win.webContents.openDevTools();
+    main.webContents.openDevTools();
   }
 
-  win.loadFile(path.join(__dirname, "./renderer/index.html"));
+  main.loadFile(path.join(__dirname, "./renderer/index.html"));
 };
+
+function aboutWindow () {
+  const about = new BrowserWindow({
+    width: 400,
+    height: 400,
+    alwaysOnTop: true,
+  });
+
+  about.setMenuBarVisibility(false);
+
+  about.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
 
 app.whenReady().then(() => {
   // Initialize Functions
   ipcMain.handle('axios.openAI', openAI);
 
+  // Create Main Window
   createWindow();
 
+  // Start Window
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -38,6 +130,7 @@ app.whenReady().then(() => {
   });
 });
 
+// Close Window
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
